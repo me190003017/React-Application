09279e731f9a1e49911cfd39ab95052cde1a792a
Bhupendra singh <bhupendrasingh62435@gmail.com>
2021-02-09 23:48:06 +0530

    we have used react router to make navigation bar and make contact page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,47 +1,45 @@
 import React, { Component } from "react";
-import Header from './HeaderComponent'
-import Footer from './FooterComponent'
-import Menu from './MenuComponent'
+import Header from "./HeaderComponent";
+import Home from './HomeComponent';
+import Footer from "./FooterComponent";
+import Menu from "./MenuComponent";
+import Contact  from "./ContactComponent";
 import { DISHES } from "../shared/dishes";
-import DishDetails from "./DishdetailComponent"
-
+import DishDetails from "./DishdetailComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null
-    }
+    };
   }
-  onDishSelect(dishId) {
-    this.setState({
-      selectedDish: dishId
-    })
-  }
+
   render() {
+
+    const HomePage = () => {
+      return (
+        <Home />
+      )
+    };
+
     return (
+
       <div>
-        <Header/>
-        <Menu dishes={this.state.dishes}
-          onClick={(dishId) => { this.onDishSelect(dishId) }} />
-        <DishDetails
-          dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-          <Footer/>
+
+        <Header />
+          <Switch>
+              <Route path="/home" component={HomePage}/>
+              <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
+              <Route exact path="/contactus" component={Contact}/>
+              <Redirect to="/home" />
+          </Switch>
+        <Footer />
+
       </div>
     );
   }
 }
 
 export default Main;
-
-
-
-
-
-
-
-
-
-
-
