64b2ff97d9dc8efaa9c82d005908b84c61f50002
Bhupendra singh <bhupendrasingh62435@gmail.com>
2021-02-10 11:25:59 +0530

    we have add home in our single page application

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,8 +3,11 @@ import Header from "./HeaderComponent";
 import Home from './HomeComponent';
 import Footer from "./FooterComponent";
 import Menu from "./MenuComponent";
-import Contact  from "./ContactComponent";
+import Contact from "./ContactComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 import DishDetails from "./DishdetailComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 
@@ -13,6 +16,9 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -20,7 +26,10 @@ class Main extends Component {
 
     const HomePage = () => {
       return (
-        <Home />
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+        />
       )
     };
 
@@ -29,12 +38,12 @@ class Main extends Component {
       <div>
 
         <Header />
-          <Switch>
-              <Route path="/home" component={HomePage}/>
-              <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
-              <Route exact path="/contactus" component={Contact}/>
-              <Redirect to="/home" />
-          </Switch>
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
 
       </div>
